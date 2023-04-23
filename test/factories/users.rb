77f2e5a3873fbf152b3b8f6657c730332585b018
Al Snow <jasnow@hotmail.com>
2023-04-23 11:24:50 -0400

    Upgraded importmap-railsr gem; fixed warning

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { "#{Faker::Name.first_name} #{Faker::Name.unique.last_name}" }
-    email { Faker::Internet.safe_email(name: name.downcase.gsub(" ",".")) }
+    email { Faker::Internet.email(name: name.downcase.gsub(" ",".")) }
     telephone { Faker::PhoneNumber.phone_number_with_country_code }
     organization
   end
